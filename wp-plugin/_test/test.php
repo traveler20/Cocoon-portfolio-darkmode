563123079b3ffe3d9d8d8693fc97580f59dc19b2
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2023-01-17 21:49:50 +0900

    📝Modify

--- wp-plugin/_test/test.php
+++ wp-plugin/_test/test.php
@@ -12,9 +12,9 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 // ──────────────────────────────
 // wp_footerで「ほげええええぇぇぇ！」と出力する
-add_action( 'wp_footer', function() {
-	echo 'ほげええええぇぇぇ！';
-});
+// add_action( 'wp_footer', function() {
+// 	echo 'ほげええええぇぇぇ！';
+// });
 
 // CSSやJSファイルの読み込ませ方
 // define( 'MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
@@ -103,13 +103,46 @@ add_action('admin_menu', function(){
 // メインメニューページ内容の表示・更新処理
 // ──────────────────────────────
 function toriaezu_mainmenu_page_contents() {
-// HTML表示
-echo <<<EOF
-<div class="wrap">
-	<h2>メインメニューです</h2>
+// ユーザーが必要な権限を持つか確認
+if (!current_user_can('manage_options'))
+{
+    wp_die( __('この設定ページのアクセス権限がありません') );
+}
+// 初期化
+$opt_name = 'toriaezu_message'; //オプション名の変数
+$opt_val = get_option( $opt_name ); // 既に保存してある値があれば取得
+$opt_val_old = $opt_val;
+$message_html = "";
+// 更新されたときの処理
+if( isset($_POST[ $opt_name ])) {
+// POST されたデータを取得
+$opt_val = $_POST[ $opt_name ];
+// POST された値を$opt_name=$opt_valでデータベースに保存(wp_options テーブル内に保存)
+update_option($opt_name, $opt_val);
+// 画面にメッセージを表示
+$message_html =<<<EOF
+<div class="notice notice-success is-dismissible">
 	<p>
-		toriaezu_menuのページです。
+		メッセージを保存しました
+		({$opt_val_old}→{$opt_val})
 	</p>
+</div>		
+EOF;		
+}
+// HTML表示
+echo $html =<<<EOF
+{$message_html}
+<div class="wrap">
+	<h2>とりあえずメニューページ</h2>
+	<form name="form1" method="post" action="">
+		<p>
+			<input type="text" name="{$opt_name}" value="{$opt_val}" size="32" placeholder="メッセージを入力してみて下さい">
+		</p>
+		<hr />
+		<p class="submit">
+			<input type="submit" name="submit" class="button-primary" value="メッセージを保存" />
+		</p>
+	</form>
 </div>
 EOF;
 }
